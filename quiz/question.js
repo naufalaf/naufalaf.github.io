a1744a4e21df116b8c599aeac6c58b9ade6a7b4f
naufalaf <nfattah86@gmail.com>
2017-06-22 09:52:12 +0700

    k00is

---
+++ quiz/question.js
@@ -0,0 +1,82 @@
+var questions = [{
+	"question": "pak Randi mempunyai sepetak kebun yang panjang nya 12 meter dan lebarnya 8 meter. Kebun itu akan dibuat pagar di sekelilingnya, maka panjang pagar itu adalah.... meter",
+	"option1": "20",
+	"option2": "28",
+	"option3": "40",
+	"option4": "44",
+	"key": "Keliling kebun = (12+8)x2 = 40",
+	"answer": "3"
+}, {
+	"question": "Rumus luas trapesium adalah....",
+	"option1": "jumlah sisi sejajar x tinggi x 2",
+	"option2": "jumlah sisi sejajar : tinggi + 2",
+	"option3": "jumlah sisi sejajar x tinggi : 2",
+	"option4": "jumlah sisi sejajar + tinggi x 2",
+	"key": "rumus luas trapesium = jumlah sisi sejajar x tinggi : 2",
+	"answer": "3"
+}, {
+	"question": "rumus luas lingkaran adalah....",
+	"option1": "π x r x r",
+	"option2": "π x r",
+	"option3": "π + r x r",
+	"option4": "π x r + r",
+	"key": "rumus luas lingkaran adalah π x r x r",
+	"answer": "1"
+}, {
+	"question": "luas lingkaran dengan jari jari 7 cm adalah....cm",
+	"option1": "155",
+	"option2": "154",
+	"option3": "153",
+	"option4": "152",
+	"key": "π x 7 x 7 = 154",
+	"answer": "2"
+}, {
+	"question": "keliling lingkaran ber jari jari 7cm adalah.... cm",
+	"option1": "44",
+	"option2": "154",
+	"option3": "22",
+	"option4": "132",
+	"key": "keliling lingkaran = 2 x π x r, 2 x 3,14 x 7 = 44",
+	"answer": "1"
+},{
+	"question": "Keliling persegi dengan sisi 8 cm adalah.....",
+	"option1": "16cm",
+	"option2": "24cm",
+	"option3": "32cm",
+	"option4": "28cm",
+	"key": "keliling persegi : 4Xsisi \n 4X8=24",
+	"answer": "3"
+	
+}, {
+	"question": "Luas bangun persegi panjang dengan panjang 8cm dan lebar 4cm adalah....",
+	"option1": "12cm",
+	"option2": "18cm",
+	"option3": "24cm",
+	"option4": "32cm",
+	"key": "luas bangun persegi panjang : panjang X lebar \n 8X4=32 ",
+	"answer": "4"
+}, {
+	"question": "Rumus luas persegi panjang adalah....",
+	"option1": "panjang : lebar",
+	"option2": "panjang - lebar",
+	"option3": "panjang X lebar",
+	"option4": "panjang + lebar",
+	"key": "rumus luas persegi panjang adalah Panjang X lebar",
+	"answer": "3"
+}, {
+	"question": "rumus luas segitiga adalah",
+	"option1": "alas X tinggi : 2",
+	"option2": "alas + tinggi : 2",
+	"option3": "alas X tinggi",
+	"option4": "alas : tinggi : 2",
+	"key": "rumus luas segitiga adalah alas X tinggi : 2",
+	"answer": "1"
+}, {
+	"question": "luas segitiga dengan alas 24cm dan tinggi 16cm adalah....",
+	"option1": "192cm",
+	"option2": "291cm",
+	"option3": "384cm",
+	"option4": "480cm",
+	"key": "",
+	"answer": "3"
+}];
